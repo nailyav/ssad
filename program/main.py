f1223432237e6ee88eaa9850ac1f68e63f2fb8d3
nailyav <66621387+nailyav@users.noreply.github.com>
2021-08-19 10:26:04 +0300

    Update main.py

--- program/main.py
+++ program/main.py
@@ -1,3 +1,5 @@
+# official link: https://github.com/nailyav/ssad/edit/main/program/main.py
+
 s = input()[::-1].lower()
 
 alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
