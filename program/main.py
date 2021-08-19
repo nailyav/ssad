89d3918fc50c01683602210e11edde484e27fc25
nailyav <66621387+nailyav@users.noreply.github.com>
2021-08-19 09:59:41 +0300

    Add files via upload

---
+++ program/main.py
@@ -0,0 +1,6 @@
+s = input()[::-1].lower()
+
+alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for letter in s:
+    print(str(alph.index(letter) + 1), end=' ', flush=True)
